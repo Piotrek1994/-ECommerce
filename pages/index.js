194490e082d61944f705f194879a229edf5037cd
PiotrPrus <pprus94@gmail.com>
2022-05-20 18:32:35 +0200

    Css styling and HeroBanner jsx

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Product, FooterBanner, HeroBanner} from '../ECommerce/components'
 
-const index = () => {
-  return (
-    <div>index</div>
+const Home = () => {
+  return (<div>
+    <HeroBanner/>
+    
+
+    <div className='products-heading'>
+      <h2>Best Selling Products</h2>
+      <p>Speakers of many variations</p>
+    </div>
+
+    <div className='products-container'>{['Product 1', 'Product2'].map((product) => product)}</div>
+
+    <FooterBanner/>
+	</div>
+    
   )
 }
 
-export default index
+export default Home
